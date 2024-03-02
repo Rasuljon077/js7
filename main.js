707cf41d12d8bf514885d6aeeabac6b61cd3a69c
rasuljon <anvarovr87@gmail.com>
2024-03-02 14:33:11 +0300

    add index.html

--- main.js
+++ main.js
@@ -1,39 +1,5 @@
-// // let a = []
-// let box = ""
-// let user = prompt("dell ")
-
-// let arr = user.split("dell ")
-
-// for (let i = 0; i < arr.length; i++) {
-
-//   if (arr[i]== "dell") {
-//     arr.split(1, 1)
-//   }
-//   console.log(arr);
-    
-//     // if (arr[i]=="add" || arr[i]=="dell") {
-        
-//     // }
-    
-// }
-// // console.log(a);
-
-// let srt = "lorem ipsum dolor sit amet. "
-// console.log(srt);
-// let a = prompt("rasuljon hasan ")
-// console.log(a);
-// let arr = srt.split('r')
-// let arrr = a.split(" ") 
-// for (let i = 0; i < srt.lenth; i++) {
-  
-//     if (condition) {
-        
-//     }
-
-// }
 
 let lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolorem minima numquam cumque facere aspernatur"
-
 
 let o = "o" 
 let a = "a"
